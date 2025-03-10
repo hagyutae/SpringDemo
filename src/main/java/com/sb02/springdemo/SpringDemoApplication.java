d4774c87a490b086bd99b59f5ab0f2999ff76a57
hagyutae <gted221@gmail.com>
2025-03-10 09:43:47 +0900

    refac: singleton / prototype component console printing

--- src/main/java/com/sb02/springdemo/SpringDemoApplication.java
+++ src/main/java/com/sb02/springdemo/SpringDemoApplication.java
@@ -11,13 +11,12 @@ public class SpringDemoApplication {
         ApplicationContext context = SpringApplication.run(SpringDemoApplication.class, args);
 
         SingletonComponent singletonComponent1 = (SingletonComponent) context.getBean("singletonComponent");
-        System.out.println(singletonComponent1.getRandomInt());
         SingletonComponent singletonComponent2 = (SingletonComponent) context.getBean("singletonComponent");
-        System.out.println(singletonComponent2.getRandomInt());
+        System.out.println("SingletonComponent random int values: " + singletonComponent1.getRandomInt() + ", " + singletonComponent2.getRandomInt());
+
 
         PrototypeComponent prototypeComponent1 = (PrototypeComponent) context.getBean("prototypeComponent");
-        System.out.println(prototypeComponent1.getRandomInt());
         PrototypeComponent prototypeComponent2 = (PrototypeComponent) context.getBean("prototypeComponent");
-        System.out.println(prototypeComponent2.getRandomInt());
+        System.out.println("PrototypeComponent random int values: " + prototypeComponent1.getRandomInt() + ", " + prototypeComponent2.getRandomInt());
     }
 }
